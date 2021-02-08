afa2c07d2284e2ba50c360ca64b83cddd7676d33
Roberto Castro <roberto.castro@utrgv.edu>
2021-02-08 12:21:16 -0600

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Roberto Castro on 2/8/21.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyBlegjWKO2cay4O2pkv2oNJFTx-zYrSwXE"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Roberto Castro on 2/6/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
