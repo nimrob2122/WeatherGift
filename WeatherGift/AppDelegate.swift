461d9dffd010b81f1f82438dadc43fe446d1993a
Roberto Castro <roberto.castro@utrgv.edu>
2021-02-09 20:54:33 -0600

    test

--- WeatherGift/AppDelegate.swift
+++ WeatherGift/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import GooglePlaces
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        GMSPlacesClient.provideAPIKey(APIkeys.googlePlacesKey)
+        
+        
         return true
     }
 
